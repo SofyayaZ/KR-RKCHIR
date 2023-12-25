0869055eb4da4f937daefa096c236e09602ba42c
SofyayaZ <144277916+SofyayaZ@users.noreply.github.com>
2023-12-25 12:26:56 +0300

    Combining filter and search

--- js/app.js
+++ js/app.js
@@ -1,20 +1,34 @@
+const genreFilter = document.getElementById('genre-filter');
+const moviesContainer = document.querySelector('.flex-container');
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const items = document.querySelectorAll('.item');
 
+// Фильтр
+genreFilter.addEventListener('change', function() {
+    const selectedGenre = this.value;
+    const searchTerm = searchInput.value.trim().toLowerCase();
+    filterMovies(selectedGenre, searchTerm);
+});
+
+function filterMovies(genre, search) {
+    const movies = moviesContainer.getElementsByClassName('item');
+    for (let movie of movies) {
+        const movieGenres = movie.getAttribute('data-genre').split(', ');
+        const title = movie.querySelector('h2').textContent.toLowerCase();
+
+        if ((genre === 'all' || movieGenres.includes(genre)) && (search === '' || title.includes(search))) {
+            movie.style.display = 'block';
+        } else {
+            movie.style.display = 'none';
+        }
+    }
+}
+
+// Поисковик
 searchForm.addEventListener('submit', function(event) {
     event.preventDefault();
-
+    const selectedGenre = genreFilter.value;
     const searchTerm = searchInput.value.trim().toLowerCase();
-
-    items.forEach(function(item) {
-        const title = item.querySelector('h2').textContent.toLowerCase();
-
-        if (title.includes(searchTerm)) {
-            item.style.display = 'block';
-        } 
-        else {
-            item.style.display = 'none';
-        }
-    });
+    filterMovies(selectedGenre, searchTerm);
 });
